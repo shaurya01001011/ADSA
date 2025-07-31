af099e1688a8005348a53adb6b0df543ad8df5a2
Shaurya Kulpe <shauryakulpe@gmail.com>
2025-07-31 10:02:57 +0530

    HashFunctionExample.java

--- HashFunctionExample.java
+++ HashFunctionExample.java
@@ -1,6 +1,4 @@
-
 public class HashFunctionExample {
-    // Simple hash table class
     static class HashTable {
         private String[] table;
         private int size;
@@ -10,25 +8,19 @@ public class HashFunctionExample {
             this.table = new String[size];
         }
 
-        // Hash function for strings
         public int hashFunction(String key) {
             int hash = 0;
-            // Sum the ASCII values of characters multiplied by their position
             for (int i = 0; i < key.length(); i++) {
                 hash += key.charAt(i) * (i + 1);
             }
-            // Use modulo to fit within table size
             return hash % size;
         }
 
-        // Insert key into hash table using linear probing
         public void insert(String key) {
             int index = hashFunction(key);
             int originalIndex = index;
-
-            // Linear probing for collision resolution
             while (table[index] != null) {
-                index = (index + 1) % size; // Move to next slot
+                index = (index + 1) % size;
                 if (index == originalIndex) {
                     System.out.println("Hash table is full!");
                     return;
@@ -38,24 +30,21 @@ public class HashFunctionExample {
             System.out.println("Inserted '" + key + "' at index " + index);
         }
 
-        // Search for a key in the hash table
         public int search(String key) {
             int index = hashFunction(key);
             int originalIndex = index;
-
             while (table[index] != null) {
                 if (table[index].equals(key)) {
                     return index;
                 }
-                index = (index + 1) % size; // Move to next slot
+                index = (index + 1) % size;
                 if (index == originalIndex) {
                     break;
                 }
             }
-            return -1; // Key not found
+            return -1;
         }
 
-        // Display the hash table
         public void display() {
             System.out.println("Hash Table Contents:");
             for (int i = 0; i < size; i++) {
@@ -65,19 +54,13 @@ public class HashFunctionExample {
     }
 
     public static void main(String[] args) {
-        // Create a hash table of size 10
         HashTable hashTable = new HashTable(10);
-
-        // Insert some keys
         hashTable.insert("Shaurya");
         hashTable.insert("banana");
         hashTable.insert("orange");
         hashTable.insert("grape");
-        hashTable.insert("apple");
-        // Display the hash table
         hashTable.display();
 
-        // Search for keys
         String searchKey = "banana";
         int index = hashTable.search(searchKey);
         System.out.println("\nSearching for '" + searchKey + "': " +
